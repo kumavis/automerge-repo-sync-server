b697267621629e45c44a5d9880d4ccec5d3e42a5
kumavis <aaron@kumavis.me>
2025-10-16 16:23:37 -1000

    feat: nix deps hash maintenance doc and workflow

--- flake.nix
+++ flake.nix
@@ -10,14 +10,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        npmDepsHash = import ./npm-deps-hash.nix;
       in {
         packages.default = pkgs.buildNpmPackage {
           pname = "@automerge/automerge-repo-sync-server";
           version = "0.3.0";
           src = ./.;
-          npmDepsHash = "sha256-mZRQb/TjRR8K8ZKKALuDNQZLmJ6G2T9+UgHY9+t6zYo=";
-          dontNpmBuild = true;
+          # set in ./npm-deps-hash.nix
+          inherit npmDepsHash;
 
+          # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/node/build-npm-package/default.nix
+          dontNpmBuild = true;
           # https://docs.npmjs.com/cli/v10/commands/npm-ci?v=true#omit
           npmInstallFlags = [ "--omit=dev" ];
         };
